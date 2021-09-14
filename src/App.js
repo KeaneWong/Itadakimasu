bedf04548b05cbf1adcaeb1e1d204b21b59ce9a2
KeaneWong <Keanewong913@gmail.com>
2021-09-14 11:43:04 -0700

    First commit; Generic blog template drafted. Banner working, layout needs to match original.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Home from './Container/Home';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import ContactUs from './Container/Contact Us'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Post from './Container/Post';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+
+      <Route path = '/' exact component = {Home}/> 
+      <Route path = '/contact-us' component = {ContactUs} />
+      <Route path = '/post/:postId' component = {Post} />
     </div>
+    </Router>
+
+
   );
 }
 
---
+++ src/Container/Contact Us/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css';
+/**
+* @author
+* @function ContactUs
+**/
+
+const ContactUs = (props) => {
+  return(
+    <div>ContactUs
+    
+    </div>
+   )
+
+ }
+
+export default ContactUs
