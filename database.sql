7a3a115a9552a520300c49fa64b606bb17c070d1
KeaneWong <Keanewong913@gmail.com>
2021-11-02 12:08:21 -0700

    Ready for deployment

--- database.sql
+++ database.sql
@@ -1,6 +1,13 @@
-CREATE DATABASE BlogRepo
+--CREATE DATABASE BlogRepo
 
 CREATE TABLE BlogPosts(
-    post_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    post_id integer,
+    description VARCHAR(255),
+    title VARCHAR(),
+    date VARCHAR(),
+    imgsrc VARCHAR(),
+    author VARCHAR(),
+    tags VARCHAR(),
+    text VARCHAR(),
+    image VARCHAR()
 );
