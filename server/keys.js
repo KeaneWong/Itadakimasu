7e157c5b5e836a8e9657d059cfb247932d44f212
KeaneWong <Keanewong913@gmail.com>
2021-11-01 17:58:52 -0700

    Attempting to host site

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    iam_access_id:'AKIAQL4K2MUHQR6ZPP4U',
-    iam_secret:'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'
+    iam_access_id:'AKIAQL4K2MUH4JOISDWP',
+    iam_secret:'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'
 };
