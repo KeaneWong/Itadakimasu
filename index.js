a6ea323fb6d41b701d68ba884bad6ddc3ac11f7d
KeaneWong <Keanewong913@gmail.com>
2021-11-03 11:53:48 -0700

    Changed filepath in indexjs

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   //server static content
   //npm run build
-  app.use(express.static(path.join(__dirname, "Itadakimasu/build")));
+  app.use(express.static("Itadakimasu/build"));
 }
 
 
